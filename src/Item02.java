0e29985758b3c1e704f62580853fc42da1a3a4ed
JeongMinYoung <43362052+jung410@users.noreply.github.com>
2021-05-10 23:49:27 +0900

    Effective java - 02.use builder pattern

--- src/Item02.java
+++ src/Item02.java
@@ -10,10 +10,21 @@
  * 두번째, Builder pattern
  *  : 필요한 객체를 직접 만드는 대신, 필수 매개변수만으로 생성자를 호출해 빌더 객체를 얻을 수 있다.*/
 
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
 class main {
     public static void main(String[] args) {
         Item02 cocaCola = new Item02.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
         System.out.println(cocaCola.toString());
+
+
+        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL).addTopping(Pizza.Topping.SAUSAGE).addTopping(Pizza.Topping.ONION).build();
+        System.out.println(pizza.toppings);
+        Calzone calzone = new Calzone.Builder().addTopping(Pizza.Topping.HAM).sauceInside().build();
+        System.out.println(calzone.toppings);
+
     }
 }
 
@@ -83,5 +94,91 @@ public class Item02 {
                 + "fat = " + this.fat + "\n"
                 + "sodium = " + this.sodium + "\n"
                 + "carbohydrate = " + this.carbohydrate + "\n";
+    }
+}
+
+/*
+* Builder pattern은 계층적으로 설계된 클래스와 함께 쓰기에 좋다.*/
+
+// 추상클래스를 하나 만들고(피자)
+abstract class Pizza{
+    // enum타입의 토핑을 가지고 있다.
+    public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+    // 토핑Set
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        // 빈 toppings를 만든다.
+        // EnumSet.nonOf : 빈 enum set을 만드는 메서드
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping){
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+        abstract Pizza build();
+
+        // 하위 클래스는 이 메서드를 재정의하여 "this"를 반환하도록 해야한다.
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
+}
+
+// Pizza를 상속받아 NyPizza class를 만든다.
+class NyPizza extends Pizza{
+    public enum Size {SMALL, MEDIUM, LARGE}
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder<Builder>{
+        public final Size size;
+
+        public Builder(Size size){
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        public NyPizza build() {
+            return new NyPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+}
+
+class Calzone extends Pizza{
+    private final boolean sauceInside;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private boolean sauceInside;
+
+        public Builder sauceInside() {
+            sauceInside = true;
+            return this;
+        }
+
+        @Override
+        public Calzone build() {
+            return new Calzone(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    private Calzone(Builder builder){
+        super(builder);
+        sauceInside = builder.sauceInside;
     }
 }
